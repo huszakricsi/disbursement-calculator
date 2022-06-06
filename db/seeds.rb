29fcfe1dd55cbed3dbc3288591c44d5d014f533d
Huszak Richard <richard.huszak@bvtech.io>
2022-06-06 12:43:05 +0200

    Database seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+Merchant.destroy_all
+Shopper.destroy_all
+
+merchants = []
+CSV.foreach('db/dataset/merchants.csv') do |row|
+  merchants.push(id: row[0], name: row[1], email: row[2], cif: row[3])
+end
+
+shoppers = []
+CSV.foreach('db/dataset/shoppers.csv') do |row|
+  shoppers.push(id: row[0], name: row[1], email: row[2], nif: row[3])
+end
+
+orders = []
+CSV.foreach('db/dataset/orders.csv') do |row|
+  orders.push(
+               id: row[0],
+               merchant_id: row[1],
+               shopper_id: row[2],
+               amount: row[3],
+               created_at: row[4],
+               completed_at: row[5]
+            )
+end
+
+Merchant.create(merchants)
+Shopper.create(shoppers)
+Order.create(orders)
